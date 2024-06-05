50c84875f18712fc7348f6987c18e9b7ca2272ee
cdragonzfr <146982306+cdragonzfr@users.noreply.github.com>
2024-06-05 09:11:43 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,76 @@
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = random_pet.rg_name.id
+  location = var.resource_group_location
+}
+
+resource "random_pet" "azurerm_mssql_server_name" {
+  prefix = "sql"
+}
+
+resource "random_password" "admin_password" {
+  count       = var.admin_password == null ? 1 : 0
+  length      = 20
+  special     = true
+  min_numeric = 1
+  min_upper   = 1
+  min_lower   = 1
+  min_special = 1
+}
+
+locals {
+  admin_password = try(random_password.admin_password[0].result, var.admin_password)
+}
+
+resource "azurerm_mssql_server" "server" {
+  name                         = random_pet.azurerm_mssql_server_name.id
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  administrator_login          = var.admin_username
+  administrator_login_password = local.admin_password
+  version                      = "12.0"
+}
+
+resource "azurerm_mssql_database" "db" {
+  name      = var.sql_db_name
+  server_id = azurerm_mssql_server.server.id
+
+}
+
+# Create a Log Analytics Workspace
+resource "azurerm_log_analytics_workspace" "example" {
+  name                = "example-workspace-${random_pet.azurerm_mssql_server_name.id}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+}
+
+# Optional: If you want to enable diagnostics to send SQL Server logs to the created Log Analytics Workspace
+resource "azurerm_monitor_diagnostic_setting" "sqlserver" {
+  name                       = "example-setting"
+  target_resource_id         = azurerm_mssql_server.server.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
+
+  log {
+    category = "SQLSecurityAuditEvents"
+    enabled  = true
+
+    retention_policy {
+      enabled = false
+      days    = 0
+    }
+  }
+
+  metric {
+    category = "AllMetrics"
+    enabled  = true
+
+    retention_policy {
+      enabled = false
+      days    = 0
+    }
+  }
+}
